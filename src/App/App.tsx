5bdc5b83ee79a7e77958495703561ca445318a81
GiselaThellipi <gthellipi@gmail.com>
2023-11-16 23:05:23 +0100

    create generic input

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,29 @@
 import { FC } from "react";
 
 // style
-import {} from "./style/App.style";
+import { Input } from "./style/App.style";
 
 const App: FC<{}> = () => {
-  return <>HI</>;
+  return (
+    <>
+      <Input
+        placeholder="Firstname"
+        type="text"
+        fontSize="12px"
+        borderbottomrightradius="20px"
+        bordertoprightradius="20px"
+        border="none"
+        width="100%"
+        height="45px"
+        backgroundcolor="#FFFFFF"
+        borderradius="10px"
+        paddingleft="5px"
+        padding="0 10px"
+        margin=" 5px 0 15px 0px"
+        required={true}
+      />
+    </>
+  );
 };
 
 export default App;
